c2adbacf8b726530a56749cc9dc52156e25315ba
oscar <oscar@ricoperez.es>
2022-01-04 20:23:44 +0100

    First commit

---
+++ src/main/java/es/ricoperez/pomodoroapirest/PomodoroApiRestApplication.java
@@ -0,0 +1,13 @@
+package es.ricoperez.pomodoroapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomodoroApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PomodoroApiRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/ricoperez/pomodoroapirest/PomodoroApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ricoperez.pomodoroapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomodoroApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
